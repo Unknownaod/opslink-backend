67bec31c56a5432139857f864c1be457c479b8bb
Unknownaod <unknown@dona.pw>
2025-11-19 06:46:23 -0500

    Update Civilian.js

--- models/Civilian.js
+++ models/Civilian.js
@@ -1,15 +1,21 @@
 import mongoose from "mongoose";
 
 const CivilianSchema = new mongoose.Schema({
-  communityId: { type: mongoose.Schema.Types.ObjectId, required: true },
+  userId: { type: mongoose.Schema.Types.ObjectId, required: true },
+  communityId: { type: mongoose.Schema.Types.ObjectId, required: false },
 
   name: { type: String, required: true },
   dob: { type: String },
   address: { type: String },
 
-  licenses: { type: Object, default: {} },
-  flags: { type: Array, default: [] },
-  warrants: { type: Array, default: [] }
+  license: { type: String, default: "Valid" },
+
+  notes: { type: String, default: "" },
+
+  warrants: { type: [String], default: [] },
+
+  vehicles: { type: [mongoose.Schema.Types.ObjectId], ref: "Vehicle", default: [] }
+
 }, { timestamps: true });
 
 export default mongoose.model("Civilian", CivilianSchema);
