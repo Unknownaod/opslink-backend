2c586c58002121e194f318ba5cd64bfbc32fa895
Unknownaod <unknown@dona.pw>
2025-12-02 10:09:06 -0500

    Update MDTUnitSession.js

--- models/MDTUnitSession.js
+++ models/MDTUnitSession.js
@@ -2,24 +2,17 @@ import mongoose from "mongoose";
 
 const MDTUnitSessionSchema = new mongoose.Schema({
   unitId: { type: mongoose.Schema.Types.ObjectId, ref: "Unit", required: true },
-  communityId: { type: mongoose.Schema.Types.ObjectId, ref: "Community", required: true },
   userId: { type: mongoose.Schema.Types.ObjectId, ref: "User", required: true },
+  communityId: { type: mongoose.Schema.Types.ObjectId, ref: "Community", required: true },
 
-  // current unit session status
   online: { type: Boolean, default: true },
-  lastStatus: { type: String, default: "10-7" },
-
-  // geolocation history
-  lastLocation: {
-    lat: Number,
-    lng: Number,
-    updatedAt: { type: Date, default: Date.now }
-  },
-
-  // timestamps for session start/end
   sessionStarted: { type: Date, default: Date.now },
   sessionEnded: { type: Date },
 
-}, { timestamps: true });
+  lastStatus: { type: String, default: "10-8" },
+  lastLocation: { lat: Number, lng: Number },
+  lastUpdate: { type: Date, default: Date.now }
+});
 
-export default mongoose.model("MDTUnitSession", MDTUnitSessionSchema);
+export default mongoose.models.MDTUnitSession ||
+       mongoose.model("MDTUnitSession", MDTUnitSessionSchema);
